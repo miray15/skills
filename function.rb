5c3429303520120983908ba29dc52a63ee2275b2
Miray Karabulut <mimikarabulut@gmail.com>
2023-07-03 13:48:25 -0500

    function - return first letter of string

--- function.rb
+++ function.rb
@@ -21,13 +21,18 @@
 
 # 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
 
-def times(number)
-  return number * number
-end 
-p times(5)
+# def times(number)
+#   return number * number
+# end 
+# p times(5)
 
 
 # 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
+
+def first_letter(string)
+  return string.chr
+end 
+p first_letter("hello")
 
 # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
 
