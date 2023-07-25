6827a16d5f31fff992bfdd1376a25ac63d47f9f7
Miray Karabulut <mimikarabulut@gmail.com>
2023-07-25 18:09:36 -0500

    reduce - turn words in string to one word with no spaces

--- reduce.rb
+++ reduce.rb
@@ -1,16 +1,22 @@
 #  1. Start with an array of numbers and compute the sum of all the numbers.
 #     For example, [5, 10, 8, 3] becomes 26.
 
-numbers = [5, 10, 8, 3]
-sum = []
-numbers.each do |number|
-  
-  sum << number 
-end 
-p sum 
+# numbers = [5, 10, 8, 3]
+# sum = 0
+# numbers.each do |number|
+# sum = sum + number  
+# end 
+# p sum 
 
 #  2. Start with an array of strings and combine them all into a single string.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+
+strings = ["volleyball", "basketball", "badminton"]
+combined = ""
+strings.each do |string|
+combined = combined + string   
+end 
+p combined 
 
 #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
