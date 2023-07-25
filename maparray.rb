bea3c94f6f16ddd16ff6617b72851c78e470683c
Miray Karabulut <mimikarabulut@gmail.com>
2023-07-25 17:58:54 -0500

    reduce - return array of numbers with all numbers added together

--- maparray.rb
+++ maparray.rb
@@ -109,12 +109,12 @@
 
 # 10. Start with an array of numbers and create a new array with each number converted into a string.
 #     For example, [1, 2, 3] becomes ["1", "2", "3"].
- numbers =  [1, 2, 3]
- string = []
- numbers.each do |number|
-string << number.to_s
- end 
-p string 
+#  numbers =  [1, 2, 3]
+#  string = []
+#  numbers.each do |number|
+# string << number.to_s
+#  end 
+# p string 
 
 # SOLUTIONS (using while loop): https://gist.github.com/peterxjang/b9ac4390aad2301a2238efc95c904f3d
 # SOLUTIONS (using .each shortcut): https://gist.github.com/peterxjang/66598fd7cdbc67fe663624e217cebbaf
