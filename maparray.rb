dd328a8455d90ca442550541a3e7930d9102a5ff
Miray Karabulut <mimikarabulut@gmail.com>
2023-07-25 17:37:52 -0500

    map array return age key from array of hashes

--- maparray.rb
+++ maparray.rb
@@ -49,15 +49,32 @@
 #  5. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
 
-strings = ["hello", "goodbye"]
-new_string = []
-strings.each do |string|
-  new_string << string.length
-end 
-p new_string
+# strings = ["hello", "goodbye"]
+# new_string = []
+# strings.each do |string|
+#   new_string << string.length
+# end 
+# p new_string
 
 #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
+
+people = 
+  [{name: "Alice", age: 27}, 
+    {name: "Blane", age: 16}]
+
+ages = []
+people.each do  |person|
+  ages << person[:age]
+end
+p ages
+
+# people = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }]
+# ages = []
+# people.each do |person|
+#   ages << person[:age]
+# end
+# p ages
 
 #  7. Start with an array of numbers and create a new array with each number divided by 2.
 #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
