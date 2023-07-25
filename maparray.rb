82ada43b7301c7c05152a90aff0db5f1515579fa
Miray Karabulut <mimikarabulut@gmail.com>
2023-07-25 17:09:07 -0500

    map array - take array of numbers and return each index + 7

--- maparray.rb
+++ maparray.rb
@@ -21,23 +21,30 @@
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
  
-hashes =
-  [
-    {name: "Alice", age: 27},
-    {name: "Blane", age: 16}
-    ]
+# hashes =
+#   [
+#     {name: "Alice", age: 27},
+#     {name: "Blane", age: 16}
+#     ]
 
-  names = []
+#   names = []
 
-  hashes.each do |hash|
-    names << hash[:name]
-  end 
-  p names
+#   hashes.each do |hash|
+#     names << hash[:name]
+#   end 
+#   p names
   
 
 
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
+
+numbers = [1, 2, 3]
+new_numbers = []
+numbers.each do |number|
+  new_numbers << number + 7
+end 
+p new_numbers
 
 #  5. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
