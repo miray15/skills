5580f27ff13fa810478b0186c4eb75f9e72876ec
Miray Karabulut <mimikarabulut@gmail.com>
2023-07-25 17:07:07 -0500

    map array - in array of hashes return only the name key

--- maparray.rb
+++ maparray.rb
@@ -11,15 +11,30 @@
 #  2. Start with an array of strings and create a new array with each string upcased.
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
 
-strings = ["hello", "goodbye"]
-new_string = []
-strings.each do |string|
-  new_string << string.upcase 
-end 
-p new_string
+# strings = ["hello", "goodbye"]
+# new_string = []
+# strings.each do |string|
+#   new_string << string.upcase 
+# end 
+# p new_string
 
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
+ 
+hashes =
+  [
+    {name: "Alice", age: 27},
+    {name: "Blane", age: 16}
+    ]
+
+  names = []
+
+  hashes.each do |hash|
+    names << hash[:name]
+  end 
+  p names
+  
+
 
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
