bdf3e0b6b833b0d81f50525fd998e5d2afdbb753
Miray Karabulut <mimikarabulut@gmail.com>
2023-07-25 17:02:25 -0500

    map arrays - make words in array upcase

--- maparray.rb
+++ maparray.rb
@@ -1,15 +1,22 @@
 #  1. Start with an array of numbers and create a new array with each number times 3.
 #     For example, [1, 2, 3] becomes [3, 6, 9].
 
-numbers = [1, 2, 3]
-new_numbers = []
-numbers.each do |number|
-  new_numbers << number * 3  
-end  
-p new_numbers
+# numbers = [1, 2, 3]
+# new_numbers = []
+# numbers.each do |number|
+#   new_numbers << number * 3  
+# end  
+# p new_numbers
 
 #  2. Start with an array of strings and create a new array with each string upcased.
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+
+strings = ["hello", "goodbye"]
+new_string = []
+strings.each do |string|
+  new_string << string.upcase 
+end 
+p new_string
 
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
