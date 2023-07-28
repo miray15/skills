87033cb8b6abdc5f50e05fe54e80c1785d6338ea
Miray Karabulut <mimikarabulut@gmail.com>
2023-07-28 15:34:54 -0500

    reduce - combined strings to one string with dashes between the words

--- reduce.rb
+++ reduce.rb
@@ -89,15 +89,23 @@
 #  7. Start with an array of numbers and compute product of all the numbers.
 #     For example, [5, 10, 8, 3] becomes 1200.
 
-numbers = [5, 10, 8, 3]
-product = 1 
-numbers.each do |number|
-  product = product * number  
-end 
-p product 
+# numbers = [5, 10, 8, 3]
+# product = 1 
+# numbers.each do |number|
+#   product = product * number  
+# end 
+# p product 
 
 #  8. Start with an array of strings and combine them all into a single string, separated by dashes.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
+
+strings = ["volleyball", "basketball", "badminton"]
+combined = ""
+strings.each do |string|
+  combined << string + "-"
+end 
+p combined
+
 
 #  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
