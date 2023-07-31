7c3592694d05dd6e803e9d1a842cefef20339e90
Miray Karabulut <mimikarabulut@gmail.com>
2023-07-30 19:30:39 -0500

    select - reduce strings to those starting with w

--- select.rb
+++ select.rb
@@ -1,19 +1,29 @@
 #  1. Start with an array of numbers and create a new array with only the numbers less than 20.
 #     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 
-numbers = [2, 32, 80, 18, 12, 3]
-less = []
+# numbers = [2, 32, 80, 18, 12, 3]
+# less = []
 
-numbers.each do |number|
-  if number < 20 
-    then less << number
-  end 
-end 
-p less 
+# numbers.each do |number|
+#   if number < 20 
+#     then less << number
+#   end 
+# end 
+# p less 
 
 
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+
+strings = ["winner", "winner", "chicken", "dinner"]
+w = []
+strings.each do |string|
+  if string[0] == "w"
+    then w << string 
+  end 
+end 
+
+p w 
 
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
