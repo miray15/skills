5dbb94b8ee77862e7711f21df26f2009cc48f8a3
Miray Karabulut <mimikarabulut@gmail.com>
2023-07-30 19:37:02 -0500

    return only strings with less than 4 letters

--- select.rb
+++ select.rb
@@ -41,16 +41,25 @@
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
 
-numbers = [2, 4, 5, 1, 8, 9, 7]
-even = []
-numbers.each do |number|
-  if number % 2 == 0 
-    even << number 
-  end 
-end 
-p even 
+# numbers = [2, 4, 5, 1, 8, 9, 7]
+# even = []
+# numbers.each do |number|
+#   if number % 2 == 0 
+#     even << number 
+#   end 
+# end 
+# p even 
 #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+
+strings =  ["a", "man", "a", "plan", "a", "canal", "panama"]
+short = []
+strings.each do |string|
+  if string.length < 4
+    then short << string 
+  end 
+end 
+p short 
 
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
