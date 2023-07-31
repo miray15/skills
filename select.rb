48b40ee709af7f9a0e599857035607d06b525368
Miray Karabulut <mimikarabulut@gmail.com>
2023-07-30 19:49:02 -0500

    select - from array of hashes return only those items with price less than 10

--- select.rb
+++ select.rb
@@ -89,17 +89,26 @@
 #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
 
-strings = ["big", "little", "good", "bad"]
-not_b = []
-strings.each do |string|
-  if not string[0] == "b"
-    then not_b << string 
-  end 
-end 
-p not_b
+# strings = ["big", "little", "good", "bad"]
+# not_b = []
+# strings.each do |string|
+#   if not string[0] == "b"
+#     then not_b << string 
+#   end 
+# end 
+# p not_b
 
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
+
+hashes = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+less_than_10 = []
+hashes.each do |hash|
+  if hash[:price] < 10
+    then less_than_10 << hash 
+  end 
+end 
+p less_than_10
 
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
