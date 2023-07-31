7bb98092a6c2cba5cb895f741b8501aec3f126f8
Miray Karabulut <mimikarabulut@gmail.com>
2023-07-30 19:46:22 -0500

    select - return only words with first letter that is not 'b'

--- select.rb
+++ select.rb
@@ -77,17 +77,26 @@
 #  7. Start with an array of numbers and create a new array with only the numbers less than 10.
 #     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
 
-numbers = [8, 23, 0, 44, 1980, 3]
-less_than_10 = []
-numbers.each do |number|
-  if number < 10 
-    then less_than_10 << number
-  end 
-end 
-p less_than_10
+# numbers = [8, 23, 0, 44, 1980, 3]
+# less_than_10 = []
+# numbers.each do |number|
+#   if number < 10 
+#     then less_than_10 << number
+#   end 
+# end 
+# p less_than_10
 
 #  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
 #     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
+
+strings = ["big", "little", "good", "bad"]
+not_b = []
+strings.each do |string|
+  if not string[0] == "b"
+    then not_b << string 
+  end 
+end 
+p not_b
 
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
