dca174a931118f0401d9c9daf484daea624e68c1
Miray Karabulut <mimikarabulut@gmail.com>
2023-08-17 15:03:14 -0500

    array - add two words

--- arrays.rb
+++ arrays.rb
@@ -1,5 +1,13 @@
 # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
 
+words = ["apple", "beautiful", "carport"]
+
+words << "desk"
+words << "elementary"
+
+p words 
+
+
 # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
 
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
