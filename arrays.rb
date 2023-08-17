4d23286f77239ca9bf658f9e46355db1c8aa5a95
Miray Karabulut <mimikarabulut@gmail.com>
2023-08-17 15:19:50 -0500

    array - store two numbers and p on separate lines using while loop

--- arrays.rb
+++ arrays.rb
@@ -58,10 +58,20 @@
 
 # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
 
-numbers = [1, 2, 3, 4, 5]
-numbers[0] = numbers[0] * 10
-p numbers 
+# numbers = [1, 2, 3, 4, 5]
+# numbers[0] = numbers[0] * 10
+# p numbers 
+
+
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+numbers = [1, 2]
+index = 0 
+while index < numbers.length 
+  p numbers[index]
+  index = index + 1 
+end 
+
 
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
 
