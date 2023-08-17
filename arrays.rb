18437680feeeea2b4da808d465caced530a5c7f7
Miray Karabulut <mimikarabulut@gmail.com>
2023-08-17 15:09:49 -0500

    array - add numbers to existing array

--- arrays.rb
+++ arrays.rb
@@ -18,15 +18,21 @@
 
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 
-numbers = [1, 2, 3, 4, 5]
-index = 0
-while index < numbers.length 
-  p numbers[index]
-  index = index + 1 
-end 
+# numbers = [1, 2, 3, 4, 5]
+# index = 0
+# while index < numbers.length 
+#   p numbers[index]
+#   index = index + 1 
+# end 
 
 
 # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+numbers = [1]
+numbers << 2
+numbers << 3
+numbers << 4
+p numbers
 
 # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
 
