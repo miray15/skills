7eaa235f0f2655880ce6de4b34a8270ab4aad386
Miray Karabulut <mimikarabulut@gmail.com>
2023-08-17 15:18:36 -0500

    array - change first number in array to itself times 10

--- arrays.rb
+++ arrays.rb
@@ -52,12 +52,15 @@
 
 # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
 
-strings = ["two", "strings"]
-strings << "three"
-p strings 
+# strings = ["two", "strings"]
+# strings << "three"
+# p strings 
 
 # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
 
+numbers = [1, 2, 3, 4, 5]
+numbers[0] = numbers[0] * 10
+p numbers 
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
