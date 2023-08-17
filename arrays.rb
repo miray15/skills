07cea9826434120825486956fddc0b988c92a43a
Miray Karabulut <mimikarabulut@gmail.com>
2023-08-17 15:05:20 -0500

    array - change second letter to number

--- arrays.rb
+++ arrays.rb
@@ -1,15 +1,19 @@
 # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
 
-words = ["apple", "beautiful", "carport"]
+# words = ["apple", "beautiful", "carport"]
 
-words << "desk"
-words << "elementary"
+# words << "desk"
+# words << "elementary"
 
-p words 
+# p words 
 
 
 # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
 
+letters = "a", "e", "i", "o"
+ letters[1] = 4
+
+ p letters 
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 
 # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
